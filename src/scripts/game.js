26564e163b0ad45291fac232b2302f787e22bf44
Kais3rP <cesare.polonara@gmail.com>
2020-11-04 04:10:31 +0100

    changed game.js

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -19,7 +19,7 @@ const config = {
     width: DEFAULT_WIDTH,
     height: DEFAULT_HEIGHT
   },
-  scene: [LoadBgScene,PreloadScene, MainScene, TestScene],
+  scene: [LoadBgScene, PreloadScene, MainScene],
   physics: {
     default: 'arcade',
     arcade: {
@@ -28,129 +28,7 @@ const config = {
     }
   }
 }
-function NOOP (){
-  console.log("NOOP")
-}
-const config2 = {
-  type: Phaser.AUTO,
-  parent: null,
-  width: 800,
-  height: 600,
-  scale: {
-      mode: Phaser.Scale.NONE,
-      autoCenter: Phaser.Scale.NO_CENTER
-  },
-  autoRound: false,
-  canvas: null,
-  canvasStyle: null,
 
-  scene: [LoadBgScene,PreloadScene, MainScene],
-
-  callbacks: {
-      preBoot: NOOP,
-      postBoot: NOOP
-  },
-
-  seed: [ (Date.now() * Math.random()).toString() ],
-
-  title: '',
-  url: 'https://phaser.io',
-  version: '',
-
-  input: {
-      keyboard: {
-          target: window
-      },
-      mouse: {
-          target: null,
-          capture: true
-      },
-      activePointers: 1,
-      touch: {
-          target: null,
-          capture: true
-      },
-      smoothFactor: 0,
-      gamepad: false,
-      windowEvents: true,
-  },
-  disableContextMenu: false,
-
-  backgroundColor: 0,
-
-  render: {
-      antialias: true,
-      antialiasGL: true,
-      mipmapFilter: 'LINEAR', // 'NEAREST', 'LINEAR', 'NEAREST_MIPMAP_NEAREST', 'LINEAR_MIPMAP_NEAREST', 'NEAREST_MIPMAP_LINEAR', 'LINEAR_MIPMAP_LINEAR'
-      pixelArt: false,
-      roundPixels: false,
-      transparent: false,
-      clearBeforeRender: true,
-      premultipliedAlpha: true,
-      preserveDrawingBuffer: false,
-      failIfMajorPerformanceCaveat: false,
-      powerPreference: 'default', // 'high-performance', 'low-power' or 'default'
-      batchSize: 4096,
-      desynchronized: false,
-      maxTextures: -1
-  },
-
-  physics: {
-      default: false  // no physics system enabled
-  },
-
-  loader:{
-      baseURL: '',
-      path: '',
-      enableParallel: true,
-      maxParallelDownloads: 4,
-      crossOrigin: undefined,
-      responseType: '',
-      async: true,
-      user: '',
-      password: '',
-      timeout: 0
-  },
-
-  images: {
-      default: '',
-      missing: ''
-  },
-
-  dom: {
-      createContainer: false,
-      behindCanvas: false,
-  },
-
-  plugins: {
-      global: [
-          //{key, plugin, start}
-      ],
-      scene: [
-          // ...
-      ]
-  },
-
-  fps: {
-      min: 10,
-      target: 60,
-      forceSetTimeOut: false,
-      deltaHistory: 10
-  },
-
-  disableContextMenu: false,
-  banner: {
-      hidePhaser: false,
-      text: '#ffffff',
-      background: [
-          '#ff0000',
-          '#ffff00',
-          '#00ff00',
-          '#00ffff',
-          '#000000'
-      ]
-  }
-}
 
 window.addEventListener('load', () => {
   const game = new Phaser.Game(config)
